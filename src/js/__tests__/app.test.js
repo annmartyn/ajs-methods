46493720d65857a3d93c91a2d9612b17dad63abe
annmartyn <yutamar@yandex.ru>
2021-12-31 17:44:47 +0300

    redone, new files and tests

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,29 +1,29 @@
 import Character from '../app.js';
 import Daemon from '../daemon.js';
 
-test('wrong type', () => {
+test('checking error - wrong type', () => {
     const char = () => new Character('Harry', 'Knight');
     expect(char).toThrow('wrong type');
 });
 
-test('wrong name', () => {
+test('checking error - wrong name', () => {
     const char = () => new Character('HarryCoolestZombie', 'Daemon');
     expect(char).toThrow('wrong name');
 });
 
-test('set damage', () => {
+test('checking set damage', () => {
     const char = new Daemon('Marry', 'Daemon');
     char.damage(12);
     expect(char.health).toBe(92.8);
 })
 
-test('level up', () => {
+test('checking level up', () => {
     const char = new Daemon('Marry', 'Daemon');
     char.levelUp();
     expect(char.level).toBe(2);
 })
 
-test('Character 0 Health ', () => {
+test('checking error - dead character', () => {
     const chart = new Daemon('Marry', 'Daemon');
     chart.health = 0;
     const t = () => chart.levelUp();
